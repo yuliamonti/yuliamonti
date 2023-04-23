644e525e49b6c210ecccd7b36b67162a95d3ca72
yulia monti <yuliamonti@gmail.com>
2023-04-22 20:34:11 -0500

    actualizando

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() { //una vez que cargue el html ejecutamos una function
-    eventListeners ();
+document.addEventListener('DOMContentLoaded', function () { //una vez que cargue el html ejecutamos una function
+    eventListeners();
 });
 //una vez que cargue el html ejecutamos una function
-function eventListeners () {
+function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
 
     mobileMenu.addEventListener('click', navegacionResponsive);
 }
-function navegacionResponsive () {
+function navegacionResponsive() {
     const navegacion = document.querySelector('.navegacion-principal');
 
     navegacion.classList.toggle('mostrar');
 }
+function actualizarFecha() {
+    var fecha = new Date();
+    var opcionesFecha = { year: 'numeric', month: 'long', day: 'numeric' };
+    var fechaFormateada = fecha.toLocaleDateString('es-ES', opcionesFecha);
+    document.getElementById('fecha').textContent = fechaFormateada;
+}
+
+actualizarFecha();
+
+setInterval(actualizarFecha, 1000 * 60 * 60 * 24); // Actualiza la fecha cada día.
